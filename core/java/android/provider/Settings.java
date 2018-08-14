9a765edc0836efc2d54e8e94eb0e27ed1aa84424
blkscorp33 <don.berzley@gmail.com>
2018-08-14 11:48:50 -0400

    Fix settings.java derp

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3950,6 +3950,15 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+        /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
 	/** Unlock keystore with fingerprint after reboot
          * @hide
          */
